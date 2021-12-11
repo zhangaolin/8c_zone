888f0eb7b8ff675d009d48742bc4178c2f573fa4
Xiahuafate <2064355274@qq.com>
2021-12-11 09:04:19 +0800

    增加了operator的函数print_result和set_info

--- OperatorSrc/Module.f90
+++ OperatorSrc/Module.f90
@@ -14,5 +14,20 @@ MODULE operator_type_module
         PROCEDURE, PASS :: print_result
         PROCEDURE, PASS :: clear
     ENDTYPE
+    CONTAINS 
+    SUBROUTINE print_result(this)
+        CLASS(opeator) :: this
+
+        WRITE(*,*) "the result is ", this%reslut
+
+    ENDSUBROUTINE
+    subroutine set_info(this,x,y)
+        CLASS(operator) :: this
+        REAL        :: x = 0.0       ! init the x
+        REAL        :: y = 0.0 
+
+        this%x = x
+        this%y = y
+    ENDSUBROUTINE
 
 ENDMODULE operator_type_module
