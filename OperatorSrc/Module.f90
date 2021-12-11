1b961953adaac1181d9f36833e32e4913b96d013
Xiahuafate <2064355274@qq.com>
2021-12-11 09:11:20 +0800

    增加了operator类的solve函数

--- OperatorSrc/Module.f90
+++ OperatorSrc/Module.f90
@@ -24,10 +24,28 @@ MODULE operator_type_module
     subroutine set_info(this,x,y)
         CLASS(operator) :: this
         REAL        :: x = 0.0       ! init the x
-        REAL        :: y = 0.0 
+        REAL        :: y = 1.0 
 
         this%x = x
         this%y = y
     ENDSUBROUTINE
+    subroutine solve(this,method)
+        CLASS(operator) :: this
+        CHARACTER :: method
+        
+        if (method == "+")then
+            this%result = this%x + this%y
+        elseif (method == "-")then
+            this%result = this%x - this%y
+        elseif (method == "*")then
+            this%result = this%x * this%y
+        elseif (method == "/")then
+            this%result = this%x / this%y
+        else
+            write(*,*) "The method is error!"
+        endif
+    ENDSUBROUTINE
+    
+
 
 ENDMODULE operator_type_module
