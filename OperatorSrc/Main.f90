53e87bd5a04ef32f94248bcabc80fd5872aecfa9
Xiahuafate <2064355274@qq.com>
2021-12-11 09:48:16 +0800

    实现了主程序通过指针实现多态的使用，首先定义基类的指针，使用时分别实例化不同对象

--- OperatorSrc/Main.f90
+++ OperatorSrc/Main.f90
@@ -2,6 +2,7 @@ include "Module.f90"
 program Main
     use operator_type_module
     use operator_plus_type_module
+    use operator_devide_type_module
 
     implicit none
 
@@ -9,6 +10,8 @@ program Main
 
     write(*,*) "========== main begin =========="
     nullify(operator_ptr)
+
+    write(*,*) "========== operator =========="
     allocate(operator :: operator_ptr) 
     call operator_ptr%set_info(10.0,2.5)
     call operator_ptr%solve("/")
@@ -16,7 +19,16 @@ program Main
     call operator_ptr%clear()
     deallocate(operator_ptr)
 
+    write(*,*) "========== operator_plus =========="
     allocate(operator_plus :: operator_ptr) 
+    call operator_ptr%set_info(10.0,2.5)
+    call operator_ptr%solve("/")
+    call operator_ptr%print_result()
+    call operator_ptr%clear()
+    deallocate(operator_ptr)
+
+    write(*,*) "========== operator_devide =========="
+    allocate(operator_devide :: operator_ptr) 
     call operator_ptr%set_info(10.0,2.5)
     call operator_ptr%solve("/")
     call operator_ptr%print_result()
