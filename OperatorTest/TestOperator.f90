8d76e931e579a4ac134cc2a830c8808657d5f6a6
Xiahuafate <2064355274@qq.com>
2021-12-11 10:09:06 +0800

    增加了关于operator所有函数的测试代码

--- OperatorTest/TestOperator.f90
+++ OperatorTest/TestOperator.f90
@@ -13,8 +13,8 @@ program TestOperator
 
     Write(*,*) "Test the operator: set_info"
     allocate(operator :: operator_ptr)
-    call operator_ptr%set_info(10.0,20.0)
-    if ((operator_ptr%x /= 10.0) .or. (operator_ptr%y /= 20.0))then
+    call operator_ptr%set_info(20.0,10.0)
+    if ((operator_ptr%x /= 20.0) .or. (operator_ptr%y /= 10.0))then
         nerror = nerror + 1
         write(*,*) "Error: the operator: set _info have problem"
         stop 999
@@ -24,6 +24,38 @@ program TestOperator
     call operator_ptr%solve("+")
     if (operator_ptr%result /= 30.0) then
         nerror = nerror + 1
+        write(*,*) "Error: the operator: x+y"
+    endif
+    call operator_ptr%solve("-")
+    if (operator_ptr%result /= 10.0)then
+        nerror = nerror + 1
+        write(*,*) "Error : the operator: x-y"
+    endif
+    call operator_ptr%solve("*")
+    if (operator_ptr%result /= 200.0)then
+        nerror = nerror + 1
+        write(*,*) "Error : the operator: x*y"
+    endif
+    call operator_ptr%solve("/")
+    if (operator_ptr%result /= 2.0)then
+        nerror = nerror + 1
+        write(*,*) "Error : the operator: x/y"
+    endif
+
+    write(*,*) "Test the operator: print_result"
+    call operator_ptr%print_result()
+    
+    write(*,*) "Test the operator: clear"
+    call operator_ptr%clear()
+    if ((operator_ptr%x /= 0.0) .or. (operator_ptr%y /= 1.0) &
+    .or. (operator_ptr%result /= 0.0))then
+        nerror = nerror + 1
+        write(*,*) "Error: the operator: clear"
+    endif
+
+    if (nerror == 0 )then
+        write(*,*) "Passed: operator"
+    endif
 
 
 end program TestOperator
